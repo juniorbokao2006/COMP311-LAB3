46e85a04967938753e7c4d7a4c20e50f9a84070a
Bokao Junior Matende <Juniorbokao2006@gmail.com>
2026-09-09 18:52:14 +0100

    Add Question 1: Countdown using while loop

---
+++ Question1.java
@@ -0,0 +1,13 @@
+public class Question1 {
+    public static void main(String[] args) {
+        int count = 10;
+
+        // Decrement count down to 1 using
+        while (count >= 1) {
+            System.out.println(count);
+            count--;
+        }
+
+        System.out.println("Liftoff!");
+    }
+}
