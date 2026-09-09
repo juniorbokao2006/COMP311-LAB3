25d54088e185e6995ea514fe58284a949f2b5306
Bokao Junior Matende <Juniorbokao2006@gmail.com>
2026-09-09 19:01:26 +0100

    Add Question 3: Print even numbers using while loop

---
+++ Question3.java
@@ -0,0 +1,11 @@
+public class Question3 {
+    public static void main(String[] args) {
+        int number = 2; // First even number in the range
+
+        // Increment by 2 to target only even integers up to 50
+        while (number <= 50) {
+            System.out.println(number);
+            number += 2;
+        }
+    }
+}
